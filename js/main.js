c7a0c399448acb9616c692771e79253d33f7328a
kfootball15 <fenster.js@gmail.com>
2017-03-11 17:38:58 -0500

    Added physics, ground, and currenttile

--- js/main.js
+++ js/main.js
@@ -35,9 +35,14 @@ class Character {
 		this.texture = loader.load( 'textures/character/f1.png' );
 		this.material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, map: this.texture, transparent: true } );
 		this.mesh = new THREE.Object3D();
-		this.drawing = new THREE.Mesh( new THREE.PlaneBufferGeometry(20,20), this.material );
-		this.sphere = new THREE.Mesh( new THREE.SphereGeometry(4,32,32), new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0x111111}))
+		this.drawing = new THREE.Mesh( new THREE.PlaneBufferGeometry(5,20), this.material );
+		//this.sphere = new THREE.Mesh( new THREE.SphereGeometry(2.5,32,32), new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0x111111}))
+		this.sphere = new Physijs.SphereMesh(
+	    new THREE.SphereGeometry(2.5,32,32),
+	    new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0x111111})
+		)
 		this.sphere.position.y = 0
+		this.sphere.castShadow = true;
 			// Add our sphere and drawing to our mesh
 		this.mesh.add(this.drawing)
 		this.mesh.add(this.sphere)
@@ -51,36 +56,47 @@ class Character {
 	getMesh(){
 		return this.mesh;
 	}
-		// Update the direction of the current motion
+	// Update the direction of the current motion
 	setDirection (controls) {
-    	// Either left or right, and either up or down
+    // Either left or right, and either up or down
     var x = controls.left ? 1 : controls.right ? -1 : 0,
         y = 0,
         z = controls.up ? 1 : controls.down ? -1 : 0;
     this.direction.set(x, y, z);
 	}
-		// Process the character motions
+	// Process the character motions
 	motion () {
-			// (if any)
+		// (if any)
 		if (this.direction.x !== 0 || this.direction.z !== 0) {
-		    	// Rotate the character
-		    this.rotate();
-			    // And, only if we're not colliding with an obstacle or a wall ...
-				    // if (this.collide()) {
-				    //     return false;
-				    // }
-			    // ... we move the character
-		    this.move();
-		    return true;	
+	    // Rotate the character
+	    this.rotate();
+		  // And, only if we're not colliding with an obstacle or a wall ...
+			  // if (this.collide()) {
+			  //     return false;
+			  // }
+		  // ... we move the character
+	    this.move();
+	    return true;	
 		}
 	}
-	currentTile (worldx) {
-		console.log("FIND CURRENT TILE AND LIGHT IT UP!")
-		// this.mesh.position
-		// for (var i = 0; i < newScene.world.mesh.children.length; i++) {
-		// 	if (newScene.world.mesh.children[i].position.x 
-		// 	newScene.world.mesh.children[i].position.z
-		// }
+	// Find users current tile
+	setCurrentTile (world) {
+		// Find the users 2D coordinates and round them to match the nearest tiles coordinates
+		let user2DCoords = {
+			x: Math.round(this.mesh.position.x/(world.tileWidth))*world.tileWidth,
+			z: Math.round(this.mesh.position.z/(world.tileWidth))*world.tileWidth
+		}
+		// Set the current tile property on this user, and set that tiles material to glow (emissive)
+		for (var i = 0; i < world.mesh.children.length; i++) {
+			// console.log(world.mesh.children[i].position.x, user2DCoords.x)
+			if (world.mesh.children[i].position.x === user2DCoords.x && world.mesh.children[i].position.z === user2DCoords.z) {
+				this.currentTile = world.mesh.children[i] // Set the users current tile
+				this.currentTile.material.emissive = new THREE.Color(); // set the glow for the users current tile
+			} else {
+				world.mesh.children[i].material.emissive = new THREE.Color(0x000000) // if it is NOT the current tile, turn the glow off;
+			}
+		}
+
 	}
 
 	// Rotate the character
@@ -125,45 +141,73 @@ class Character {
 class World {
 	constructor (x, y, tileWidth) {
 
-		// Function for creating a grid, called later
-		function createGrid (x, z, tileWidth, that) {
-			for (var i = x; i > -x;) {
-				for (var j = z; j > -z;){
+		// Both x and y represent the distance from (0, 0) to the edge of the game board.
+		// So both 2x and 2y are equal to the total board distance
 
-					// Create the mesh for individual tiles...
-					let material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111} );
-					let geometry = new THREE.PlaneBufferGeometry( tileWidth, tileWidth )
-					let tile = new THREE.Mesh( geometry, material );
+		// Create the Grid Meshes
+		this.createGrid(x, y, tileWidth)
 
-					// Tile Properties
-					tile.position.x = j
-					tile.position.z = i
-					tile.position.y = -10;
-					tile.rotation.x = - Math.PI / 2;
-					tile.receiveShadow = true;
-
-					// Add tile to the world mesh
-					that.mesh.add(tile)
-
-					j -= tileWidth + .1;
-
-				}
-
-				i -= tileWidth + .1;
-
-			}
-		}
-
-		// Create a mesh that will contain a mesh for each tile
-		this.tileWidth = tileWidth // we set this property so we can grab it in other funcitons if neccessary
-		this.mesh = new THREE.Object3D();
-
-		// Create the grid
-		createGrid(x, y, tileWidth, this)
+		// Create the Ground Mesh (for Physics)
+		this.createGround(x*2) 
 
 	}
-	getMesh() {
+	createGrid (x, z, tileWidth) {
+		// Create parent mesh for tiles
+		this.mesh = new THREE.Object3D();
+		// Create tiles and append them to parent
+		for (var i = x; i > -x;) {
+			for (var j = z; j > -z;){
+
+				// Create the mesh for individual tiles...
+				let material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111} );
+				let geometry = new THREE.PlaneGeometry( tileWidth, tileWidth, 1 )
+				let tile = new THREE.Mesh( geometry, material, 0/* Mass of zero*/ );
+
+				// Tile Properties
+				tile.position.x = j
+				tile.position.z = i
+				tile.position.y = -9.99;
+				tile.rotation.x = - Math.PI / 2;
+				tile.receiveShadow = true;
+
+				// Add tile to the world mesh
+				this.mesh.add(tile)
+
+				j -= tileWidth;
+			}
+
+			i -= tileWidth;
+		}
+	}
+	createGround (boardWidth) {
+		// Material
+		this.ground_material = Physijs.createMaterial(
+			new THREE.MeshLambertMaterial({ color: 0xff0000 }),
+			.8, // high friction
+			.4 // low restitution
+		);
+
+		// Geometry
+		this.ground_geometry = new THREE.PlaneGeometry( boardWidth*2, boardWidth*2, 100, 100 );
+		this.ground_geometry.computeFaceNormals();
+		this.ground_geometry.computeVertexNormals();
+
+		// If your plane is not square as far as face count then the HeightfieldMesh
+		// takes two more arguments at the end: # of x faces and # of z faces that were passed to THREE.PlaneMaterial
+		this.groundMesh = new Physijs.HeightfieldMesh(
+				this.ground_geometry,
+				this.ground_material,
+				0 // mass
+		);
+		this.groundMesh.rotation.x = -Math.PI / 2;
+		this.groundMesh.position.y = -10
+		// this.scene.add( this.groundMesh );
+	}
+	getMesh () {
 		return this.mesh;
+	}
+	getGroundMesh () {
+		return this.groundMesh
 	}
 }
 
@@ -173,10 +217,18 @@ class World {
 // ----------------------------- //
 class Box {
 	constructor (x, y, z) {
+
+		// Create a PhysiJS box
 	  this.mesh = new Physijs.BoxMesh( // We use Physijs library to create a box with physics.
 	    new THREE.CubeGeometry( x, y, z ),
 	    new THREE.MeshBasicMaterial({ color: 0xfc7b7b })
 		)
+
+		// Event Handler for when boxes are added to scene
+		this.mesh.addEventListener('ready', function(){
+			// Handle new boxes here...
+			console.log("New Box Added to Scene")
+		})
 	}
 	getMesh() {
 		return this.mesh;
@@ -203,14 +255,13 @@ class testSphere {
 // ----------- //
 class Scene {
 	constructor () {
-
-		
 		// Renderer
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize( window.innerWidth, window.innerHeight );
+    // this.renderer.shadowMap.enabled = true;
     document.getElementById( 'viewport' ).appendChild( this.renderer.domElement );
     // Physijs.Scene
-    this.scene = new Physijs.Scene;;
+    this.scene = new Physijs.Scene;
     // Camera
     this.camera();
     // Lights
@@ -219,6 +270,10 @@ class Scene {
     this.createObjects();
     // Start the event handlers
     this.setControls();
+    // Initialize Physics;
+    this.initializePhysics();
+    // State that Scene is ready for first block
+    this.readyForNextBlock = true;
     // Create window resize function:
     let that = this
 		function onWindowResize () {
@@ -241,60 +296,39 @@ class Scene {
     this.scene.add( this.camera );
 	}
 	lights () {
-		// Lights
-	  // var light, materials;
-
 	  // Ambient
 	  // this.scene.add( new THREE.AmbientLight(0xffffff, .01) )
 
 	  // Point
-	  this.pointLight = new THREE.PointLight(0xffffff, 1, 10000);
+	  this.pointLight = new THREE.PointLight(0xffffff, 1.1, 10000);
 	  this.pointLight.position.y = 100;
 	  this.pointLight.position.x = -100;
 	  this.pointLight.position.z = -100;
+	  this.pointLight.castSahdow = true;
 	  this.scene.add(this.pointLight)
-
-
-	  //  // Directional
-	  // this.directionalLight = new THREE.DirectionalLight( 0xdfebff, 1.75 );
-		// this.directionalLight.position.set( 5, 5, 5 );
-		// this.directionalLight.position.multiplyScalar( 1.3 );
-		// this.directionalLight.castShadow = true;
-		// this.directionalLight.shadow.mapSize.width = 1024;
-		// this.directionalLight.shadow.mapSize.height = 1024;
-
-		// var d = 300;
-
-		// this.directionalLight.shadow.camera.left = - d;
-		// this.directionalLight.shadow.camera.right = d;
-		// this.directionalLight.shadow.camera.top = d;
-		// this.directionalLight.shadow.camera.bottom = - d;
-
-		// this.directionalLight.shadow.camera.far = 1000;
-
-		// this.scene.add(this.directionalLight)
 	}
 	initializePhysics () {
 		this.scene.simulate(); // run the physics
 	}
 	createObjects () {
+		// World
+		let boardWidth = 100; // Set the number of tiles Across here;
+		let tileWidth = 5; // Set the tileWidth here;
+		this.world = new World(boardWidth/2, boardWidth/2, tileWidth);
+		this.world.boardWidth = boardWidth; // Number of tiles across
+		this.world.tileWidth = tileWidth; // Individual tile Width
+		this.scene.add( this.world.getMesh() );		
 
-		// World - Create the world and add it
-		this.world = new World(50, 50, 5)
-		this.scene.add( this.world.getMesh() );			
-	  
+		// Ground
+		this.scene.add( this.world.getGroundMesh() );
+
 	  // Character - Create a new Character and add to scene.
 		this.user = new Character()
 		this.scene.add( this.user.getMesh() );
 
-	  // Falling Box Example
-	  this.boxMesh = new Box(5,5,5)
-	  this.scene.add( this.boxMesh.getMesh() );
-
 	  // Test Sphere
-	  this.testsphere = new testSphere(4, 20, 20);
-	  this.scene.add( this.testsphere.getMesh() )
-
+	  // this.testsphere = new testSphere(4, 20, 20);
+	  // this.scene.add( this.testsphere.getMesh() )
 	}
 	setControls () {
 		var user = this.user, // so we can access this inside eventlistener
@@ -368,18 +402,35 @@ class Scene {
 	frame () {
 		'use strict'
 		this.user.motion();
-		this.user.currentTile();
+		this.user.setCurrentTile(this.world);
 		this.renderer.render(this.scene, this.camera)
 	}
 }
 
-// Window Resize function (called in Scene class constructor)
+function generateBox () {
 
+}
+
+// Game logic
+function nextBlock (scene) {
+	newScene.readyForNextBlock = false;
+
+  let boxMesh = new Box(scene.world.tileWidth, scene.world.tileWidth, scene.world.tileWidth)
+  boxMesh.mesh.position.y = 60;
+  boxMesh.mesh.position.x = 0;
+  boxMesh.mesh.position.z = 0;
+  scene.scene.add( boxMesh.getMesh() );
+
+	setTimeout(function(){
+		scene.readyForNextBlock = true;
+	},1000)
+}
 
 // Here is where we initalize everything
 var newScene = new Scene()
 function animate () {
 	newScene.scene.simulate() // run the physics
+	if (newScene.readyForNextBlock) nextBlock(newScene);
 	requestAnimationFrame(animate);
 	newScene.frame();
 }
